ef73c2a99071551c3a9dfeae8051252a803890f2
kechuarceus <jesus2sanchezmacias@gmail.com>
2018-09-20 11:26:42 -0700

    endpoint added

--- src/components/compiler.vue
+++ src/components/compiler.vue
@@ -2,7 +2,7 @@
     <div>
         <div class = "columns">
             <div class = "column is-10" id = "boxC">
-
+                {{msg}}
             </div>
             <div class = "column is-1">
             </div>
@@ -11,7 +11,7 @@
             <div class = "column is-10">
                 <div class = "columns">
                     <div class = "column is-1">
-                        <button class = "button is-success is-outlined" ><div><sui-icon size="small" name="play"/>&nbsp</div> RUN </button>
+                        <button v-on:click = "click()" class = "button is-success is-outlined" ><div><sui-icon size="small" name="play"/>&nbsp</div> RUN </button>
                     </div>
                     <div class = "column is-5 is-offset-1">
                             <button class = "button is-info is-outlined"><div><sui-icon  name="chevron left"/></div></button>
@@ -29,11 +29,31 @@
 </template>
 
 <script>
+ //import $ from "jquery";
+var $ = window.jQuery = require("jquery");
     export default{
         name: "compiler",
         data(){
             return{
+                msg: "hello there"
+            }
+        },
+        methods:{
+            click: function(){
+                // $.get("https://compass.ucmerced.edu/pluto/date", {}, function(res){
+                //     var obj = JSON.parse(res)
+                //     this.msg = obj.date
+                // })
 
+                $.ajax({
+                    method: "GET", 
+                    data: {},
+                    url: "https://compass.ucmerced.edu/pluto/datep",
+                    success:  function(res){
+                        console.log(res);
+                    },
+                    dataType: "jsonp"
+                })
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+//import $ from "jquery";
+
 //IMPORT CSS
 import "./assets/css/sidebar.css";
 import "./assets/css/compiler.css";
