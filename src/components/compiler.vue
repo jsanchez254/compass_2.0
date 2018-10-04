421695d7026281ae41f3f60b8fd11e9e83ac57c3
Jesus Sanchez <jsanchez254@ucmerced.edu>
2018-10-04 01:11:16 -0700

    better fetching, code displays on text editor

--- src/components/compiler.vue
+++ src/components/compiler.vue
@@ -3,8 +3,9 @@
         <br/>
         <div class = "columns">
             <div class = "column is-10">
-                <editor v-bind:content = "content1" v-on:change = "change" 
-                v-on:paste = "paste" height="500px" theme = "twilight" lang = "c_cpp"/>
+                <editor v-if = "this.content.slide == undefined"
+                v-model:content = "code" v-bind:content = "this.content.slides[0].code" v-on:change = "change" 
+                 height="500px" theme = "twilight" lang = "c_cpp"/>
             </div>
             <div class = "column is-1">
             </div>
@@ -16,23 +17,22 @@
                         <button v-on:click = "display()" class = "button is-success is-outlined" ><div><sui-icon size="small" name="play"/>&nbsp</div> RUN </button>
                     </div>
                     <div class = "column is-5 is-offset-1">
-                            <button class = "button is-info is-outlined"><div><sui-icon  name="chevron left"/></div></button>
-                            <button class = "button is-info is-outlined"><div><sui-icon  name="chevron right"/></div></button>
+                            <button v-on:click = "previousLesson()" class = "button is-info is-outlined"><div><sui-icon  name="chevron left"/></div></button>
+                            <button v-on:click = "nextLesson()" class = "button is-info is-outlined"><div><sui-icon  name="chevron right"/></div></button>
                     </div>
                 </div>
             </div>
         </div>
         <div class = "columns">
             <div class = "column is-10" id = "boxD">
-                <span v-for="currency in contento">
-                    {{currency.description}}
-                    <br/>
-                </span>
+            <span v-for = "values in content">
+               {{values}} <br/>
+            </span>
             </div>
         </div>
+        <br/>
     </div>
 </template>
-
 
 <script>
 //IMPORT AXIOS
@@ -50,30 +50,55 @@ import 'brace/theme/chrome';
         name: "compiler",
         data(){
             return{
-                msg: "hello there",
-                contento: "",
-                content1: "int hello = 7"
+                content: '',         //used to store content of get request 
+                code: "int hi = 7",       //used to store code portion
+                currentId: 1
             }
         },
-        //sample using axios
         mounted(){
-            axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            var url = 'http://localhost:5000/presentation/' + this.currentId;
+            axios.get(url)
             .then(response => (
-                this.contento = response.data.bpi
-                ))
+                this.content = response.data
+            ));
+            console.log(this.content);
+            console.log(this.content.id);
         },
         methods:{
-            //cool function that will update variable everytime change happens
+            //UPDATE CODE VARIABLE STORING CONTENT OF EDITOR
             change: function(log){
-                console.log(log);
+                this.code = log;
             },
-            paste: function() {
-                console.log('paste');
-            },
+            //DISPLAY CONTENT FROM ACE EDITOR
             display: function(){
-                //will get info from ace editor
-                let code = this.$children[0].getValue();
-                console.log(code);
+                
+                console.log(this.content.slides[0].code);
+                console.log(this.content.id);
+                console.log(this.content);
+                console.log(this.code);
+                this.code = "hello1";
+                console.log(this.code);
+                
+            },
+            //NEXT
+            nextLesson: function(){
+                this.currentId+=1;
+                this.fetchData();
+            },
+            //PREVIOUS
+            previousLesson: function(){
+                if(this.currentId > 1){
+                    this.currentId-=1;
+                }
+                this.fetchData();
+            },
+            //FETCH LESSONS FROM SERVER
+            fetchData: function(){
+                var url = 'http://localhost:5000/presentation/' + this.currentId;
+                axios.get(url)
+                .then(response => (
+                    this.content = response.data
+                ));
             }
         }
     }
